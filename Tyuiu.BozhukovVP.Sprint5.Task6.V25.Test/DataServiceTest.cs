ae5e03cc4165bebce4703ef063fb1c9e34fe2e67
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-11-27 17:59:03 +0500

    Task6

---
+++ Tyuiu.BozhukovVP.Sprint5.Task6.V25.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.BozhukovVP.Sprint5.Task6.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
