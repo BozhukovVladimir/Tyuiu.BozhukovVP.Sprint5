641362d1096ea1a5767e663ea324f09130a5166e
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-11-27 17:19:41 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.BozhukovVP.Sprint5.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.BozhukovVP.Sprint5.Task0.V5.Lib;
+namespace Tyuiu.BozhukovVP.Sprint5.Task0.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BozhukovVP.Sprint5.Task0.V5/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.BozhukovVP.Sprint5.Task0.V5.Lib;
+namespace Tyuiu.BozhukovVP.Sprint5.Task0.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Божуков В.П. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл.                       *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Божуков Владимир Павлович | ИБКСб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в  *");
+            Console.WriteLine("* текстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить      *");
+            Console.WriteLine("* до трёх знаков после запятой.                                           *");
+            Console.WriteLine("* y = ln((x+1)/(x+2))                                                     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            int x = 3;
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
