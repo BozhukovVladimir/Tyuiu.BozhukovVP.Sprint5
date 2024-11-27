2e6a7ae91801c9f4c2818d36a25102e5f80bc4be
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-11-27 17:37:15 +0500

    Task3

---
+++ Tyuiu.BozhukovVP.Sprint5.Task3.V8.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.BozhukovVP.Sprint5.Task3.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BozhukovVP.Sprint5.Task3.V8/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.BozhukovVP.Sprint5.Task3.V8.Lib;
+namespace Tyuiu.BozhukovVP.Sprint5.Task3.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Божуков.В.П | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнила: Божуков Владимир Павлович | ИБКСб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение (x^2+1)/(3x+4)                                           *");
+            Console.WriteLine("* вычислить его значение при x = 3, результат сохранить в бинарный файл   *");
+            Console.WriteLine("* OutPutFileTask3.bin и вывести на консоль.                               *");
+            Console.WriteLine("***************************************************************************");
+            int x = 3;
+            DataService dataService = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("x = " + x);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = dataService.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+
+            Console.ReadKey();
+        }
+    }
+}
