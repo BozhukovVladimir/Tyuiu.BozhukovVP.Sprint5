b8be2ef131725baa56d882e0cc5c7d3fe4146f6e
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-11-27 17:25:50 +0500

    Task1

---
+++ Tyuiu.BozhukovVP.Sprint5.Task1.V12.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.BozhukovVP.Sprint5.Task1.V12.Lib;
+namespace Tyuiu.BozhukovVP.Sprint5.Task1.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BozhukovVP.Sprint5.Task1.V12/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.BozhukovVP.Sprint5.Task1.V12.Lib;
+namespace Tyuiu.BozhukovVP.Sprint5.Task1.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startValue = -5;
+            int stopValue = 5;
+
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Божуков В. П. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Класс File. Запись набора данных в текстовый файл                       *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Божуков Владимир Павлович | ИБКСб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана функция, (произвести табулирование) функции на заданном диапазоне  *");
+            Console.WriteLine("* [-5, 5] с шагом 1. Результат сохранить в текстовый файл                 *");
+            Console.WriteLine("* OutPutFileTask0.txt и вывести на консоль в таблицу. Значения округлить  *");
+            Console.WriteLine("* до двух знаков после запятой.                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
