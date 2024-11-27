cec2490722d16c0198db7f55c0796df7e9eb2d07
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-11-27 18:05:49 +0500

    Task7

---
+++ Tyuiu.BozhukovVP.Sprint5.Task7.V17.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.BozhukovVP.Sprint5.Task7.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BozhukovVP.Sprint5.Task7.V17/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.BozhukovVP.Sprint5.Task7.V17.Lib;
+namespace Tyuiu.BozhukovVP.Sprint5.Task7.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт №5 | Выполнил: Божуков В.П | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Божуков Владимир Павлович  | ИБКСб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл в котором есть набор символьных данных. Удалить все удвоенные  *");
+            Console.WriteLine("* буквы 'нн' из файла .Полученный результат в OutPutDataFileTask7V17.txt. *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"C:\DataSprint5\InPutDataFileTask7V17.txt";
+            string pathSaveFile = $@"C:\DataSprint5\InPutDataFileTask7V17.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            pathSaveFile = ds.LoadDataAndSave(path);
+            Console.WriteLine(pathSaveFile);
+            Console.ReadKey();
+        }
+    }
+}
